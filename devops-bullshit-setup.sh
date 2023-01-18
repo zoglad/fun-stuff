ae9ca9d0c8a647105942bac80f5361349cbafd26
Avi Uziel <avi.uziel@aidoc.com>
2023-01-18 17:54:52 +0200

    fix alias

--- devops-bullshit-setup.sh
+++ devops-bullshit-setup.sh
@@ -3,6 +3,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 RESOURCE_DIR="$SCRIPT_DIR/resources"
 
-alias too='afplay $RESOURCE_DIR/too-too.mp3'
-alias koko='afplay $RESOURCE_DIR/kokoriko.mp3'
+alias too="afplay $RESOURCE_DIR/too-too.mp3"
+alias koko="afplay $RESOURCE_DIR/kokoriko.mp3"
 
